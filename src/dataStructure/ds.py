72a8165485b158579252a71276047155733c198a
devhoodit <devhoodit@naver.com>
2022-03-11 04:54:14 +0900

    MaxHeap, MinHeap

--- src/dataStructure/ds.py
+++ src/dataStructure/ds.py
@@ -1,3 +1,5 @@
+from collections.abc import Sequence
+
 class Stack():
     def __init__(self, length):
         self.stack = []
@@ -54,4 +56,93 @@ class  ArrayStack():
             tmp = self.stack[self.cur_pos]
             self.stack[self.cur_pos] = None
             return tmp
-            
+
+class Heap():
+    class Heap():
+        def __init__(self):
+            self.heap = [None]
+            self.size = 0
+
+        def __len__(self):
+            return self.size
+        
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            if self.size <= 0:
+                raise StopIteration
+            return self.pop()
+
+        def is_empty(self):
+            return self.size <= 0
+
+    class MaxHeap(Heap):
+        def push(self, value):
+            self.heap.append(value)
+            self.size += 1
+            i = self.size
+            while i != 1 and value > self.heap[i//2]:
+                self.heap[i], self.heap[i//2] = self.heap[i//2], self.heap[i]
+                i //= 2
+        
+        def pop(self):
+            if self.is_empty():
+                raise IndexError()
+            tmp = self.heap[1]
+            self.heap[1] = self.heap[-1]
+            self.heap.pop()
+            self.size -= 1
+
+            parent = 1
+            child = 2
+
+            while child <= self.size:
+
+                if child+1 < self.size and self.heap[child] < self.heap[child+1]: #check parent node have enough child node(2 nodes), and find which child node is bigger(guarantee parent node is bigger than child nodes when swap)
+                    child += 1
+                
+                if self.heap[child] > self.heap[parent]:
+                    self.heap[child], self.heap[parent] = self.heap[parent], self.heap[child]
+                else:
+                    break
+
+                parent = child
+                child *= 2
+
+            return tmp
+
+    class MinHeap(Heap):
+        def push(self, value):
+            self.heap.append(value)
+            self.size += 1
+            i = self.size
+            while i != 1 and value < self.heap[i//2]:
+                self.heap[i], self.heap[i//2] = self.heap[i//2], self.heap[i]
+                i //= 2
+
+        def pop(self):
+            if self.is_empty():
+                raise IndexError()
+            tmp = self.heap[1]
+            self.heap[1] = self.heap[-1]
+            self.heap.pop()
+            self.size -= 1
+
+            parent = 1
+            child = 2
+
+            while child < self.size:
+
+                if child+1 < self.size and self.heap[child] > self.heap[child+1]:
+                    child += 1
+                
+                if self.heap[child] < self.heap[parent]:
+                    self.heap[child], self.heap[parent] = self.heap[parent], self.heap[child]
+                else:
+                    break
+
+                parent = child
+                child *= 2
+
+            return tmp
